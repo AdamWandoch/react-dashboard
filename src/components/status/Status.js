eb02652fe06d01af0616579f29ef1c5da27493a8
Adam Wandoch <wandoch.adam@gmail.com>
2022-06-20 12:01:20 +0100

    started data fetching and rendering

--- src/components/status/Status.js
+++ src/components/status/Status.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './status-style.css';
 
-export const Status = () => {
-  return <div>Status</div>;
+export const Status = ({
+  id,
+  lineName,
+  sensor1temp,
+  sensor2temp,
+  status,
+  currentProduct,
+  outputRate,
+}) => {
+  return (
+    <div className='status'>
+      <p>
+        [{lineName}] {status}: {currentProduct} @ {outputRate} units per second
+      </p>
+      <p>Sensors: {sensor1temp}°C | {sensor2temp}°C</p>
+    </div>
+  );
 };
--- src/components/statuses/Statuses.js
+++ src/components/statuses/Statuses.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { useFetcher } from '../../helpers/useFetcher';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Status } from '../status/Status';
 import './statuses-style.css';
 
 export const Statuses = () => {
+  const [data, setData] = useState([]);
+  const API_URL = 'http://localhost:5000/factory/v1/getstatus';
 
-  // useFetcher to be implemented to get data from api
-  const { data } = useFetcher();
+  useEffect(() => {
+    const getStatuses = async () => {
+      const response = await axios.get(API_URL);
+      setData(response.data);
+    };
+    setInterval(() => {
+      getStatuses();
+    }, 1000);
+  }, []);
 
-  return <>Statuses</>;
+  return (
+    <>
+      {data.map((status) => (
+        <Status key={status.id} {...status} />
+      ))}
+    </>
+  );
 };
